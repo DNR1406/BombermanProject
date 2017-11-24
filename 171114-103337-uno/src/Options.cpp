3e8f3370cfed2ca1159af51cb5bce1fab1b31500
Fjir <antalvravensteijn@hotmail.com>
2017-11-24 12:08:04 +0100

    Pulling new changes

--- 171114-103337-uno/src/Options.cpp
+++ 171114-103337-uno/src/Options.cpp
@@ -38,7 +38,7 @@ void Options::changeBrightness()
     // Background and back to options button
     lcd.fillScreen(RGB(160, 182, 219));
     lcd.drawText(80, 20, "BRIGHTNESS", RGB(0, 0, 0), RGB(160, 182, 219), 2);
-    
+
     lcd.drawText(20, 100, "Turn potentiometer knob to change the ", RGB(0, 0, 0), RGB(160, 182, 219), 1);
     lcd.drawText(20, 110, "brightness of the screen.", RGB(0, 0, 0), RGB(160, 182, 219), 1);
     lcd.drawText(20, 120, "After you're done, press the screen", RGB(0, 0, 0), RGB(160, 182, 219), 1);
@@ -49,34 +49,18 @@ void Options::changeBrightness()
     DDRC = 0b11111110;
     while (counter)
     {
-<<<<<<< HEAD
-        lcd.touchRead();
-        if (lcd.touchZ())
-        {
-            if ((lcd.touchX() > 0 && lcd.touchX() < 50) && (lcd.touchY() > 0 && lcd.touchY() < 50))
-            {
-                homeScreen.showOptions();
-                homeScreen.checkOptionsButton();
-                counter = 0;
-            }
-        }
-=======
->>>>>>> 76be74722dc612efbc6f5a82c1cb0b018ed27a72
         int val = analogRead(DDC0);
         val = map(val, 0, 1023, 0, 100);
-        if (val < 10){
+        if (val < 10)
+        {
             val = 10;
         }
 
         lcd.led(val);
-<<<<<<< HEAD
-=======
         lcd.touchRead();
         if (lcd.touchZ())
         {
             counter = 0;
         }
-        
->>>>>>> 76be74722dc612efbc6f5a82c1cb0b018ed27a72
     }
 }
