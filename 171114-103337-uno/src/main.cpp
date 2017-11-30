d99137b58684f4d16d08dd8b6cb9b372fc1dd741
Arno <s1104802@student.windesheim.nl>
2017-11-30 10:52:19 +0100

    communication add some map send functions

--- 171114-103337-uno/src/main.cpp
+++ 171114-103337-uno/src/main.cpp
@@ -6,6 +6,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
+#include <SoftwareSerial.h>
 
 #include "include.h"
 #include <Arduino.h>
@@ -15,24 +16,29 @@
 ArduinoNunchuk nunchuk = ArduinoNunchuk();
 Navigation navigation = Navigation();
 
+void testCommunication()
+{
 
-void testCommunication() {
-
-  Communication c = Communication(1, 1);
+    Communication c = Communication(1, 1);
 
     int positions[58];
-    
+
     c.receiveMap(positions);
     Serial.println("ontvangen");
 
     c.sendMap(positions);
 }
 
-
-
 int main()
 {
     init();
+
+    const byte rxPin = 0;
+    const byte txPin = 1;
+
+    // set up a new serial object
+    SoftwareSerial mySerial(rxPin, txPin);
+
     Serial.begin(9600);
 
     //testCommunication();
@@ -40,9 +46,7 @@ int main()
     navigation.screenInit();
     navigation.calibrateScreen();
 
-
     navigation.drawStartscreenButtons();
-
 
     return 0;
 }
